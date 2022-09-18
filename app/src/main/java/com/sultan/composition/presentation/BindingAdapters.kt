d259c23f9be31a41f946bce33f8076a082e21278
Sultan-assasin <sulltanadai@gmail.com>
2022-09-18 20:24:23 +0600

    создали bindingAdapter , исползывали dataBinding . все методы в BindingAdapters.kt , а код стал два раза меньше . что бы не было пробелм с viewBinding надо добавить latout . получили доступ к viewModel через data . внутри функции BindingAdapters.kt нельзя исползывать лямбда выражения б потому что они становяться типами object . через коллекции вернется оболочка элемента  , примитивный тип который нету метода toString() . что бы обновлять данные правильно и что бы при перевороте все сохранялось надо определить  binding.viewModel = viewModel  binding.lifecycleOwner =viewLifecycleOwner .  Можно установить слушатель на клик через интерфейс

--- app/src/main/java/com/sultan/composition/presentation/BindingAdapters.kt
+++ app/src/main/java/com/sultan/composition/presentation/BindingAdapters.kt
@@ -1,54 +1,101 @@
 package com.sultan.composition.presentation
 
+import android.content.Context
+import android.content.res.ColorStateList
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import com.sultan.composition.R
 import com.sultan.composition.domain.entity.GameResult
 
+interface OnOptionClickListener {
+
+    fun onOptionClick(option: Int)
+}
+
 @BindingAdapter("requiredAnswers")
-fun bindRequireAnswer(textView: TextView, count: Int) {
+fun bindRequiredAnswers(textView: TextView, count: Int) {
     textView.text = String.format(
         textView.context.getString(R.string.required_score),
         count
     )
 }
 
-@BindingAdapter("scoreAnswers")
-fun bindScoreAnswers(textView: TextView, count: Int) {
+@BindingAdapter("score")
+fun bindScore(textView: TextView, score: Int) {
     textView.text = String.format(
         textView.context.getString(R.string.score_answers),
-        count
+        score
     )
 }
 
 @BindingAdapter("requiredPercentage")
-fun bindRequirePercentage(textView: TextView, count: Int) {
+fun bindRequiredPercentage(textView: TextView, percentage: Int) {
     textView.text = String.format(
         textView.context.getString(R.string.required_percentage),
-        count
+        percentage
     )
 }
-@BindingAdapter("emojiResult")
-fun emojiResult(image : ImageView,gameResult: GameResult){
-    image.setImageResource(getSmileResId(gameResult))
+
+@BindingAdapter("scorePercentage")
+fun bindScorePercentage(textView: TextView, gameResult: GameResult) {
+    textView.text = String.format(
+        textView.context.getString(R.string.score_percentage),
+        getPercentOfRightAnswers(gameResult)
+    )
 }
 
-private fun getSmileResId(gameResult: GameResult): Int {
-    return if (gameResult.winner) {
+private fun getPercentOfRightAnswers(gameResult: GameResult) = with(gameResult) {
+    if (countOfQuestions == 0) {
+        0
+    } else {
+        ((countOfRightAnswers / countOfQuestions.toDouble()) * 100).toInt()
+    }
+}
+
+@BindingAdapter("resultEmoji")
+fun bindResultEmoji(imageView: ImageView, winner: Boolean) {
+    imageView.setImageResource(getSmileResId(winner))
+}
+
+private fun getSmileResId(winner: Boolean): Int {
+    return if (winner) {
         R.drawable.ic_smile
     } else {
         R.drawable.ic_sad
     }
 }
-@BindingAdapter("scorePercentage")
-fun scorePercentage(textView: TextView , gameResult: GameResult){
-    getPercentOfRightAnswers(gameResult)
+
+@BindingAdapter("enoughCount")
+fun bindEnoughCount(textView: TextView, enough: Boolean) {
+    textView.setTextColor(getColorByState(textView.context, enough))
 }
-private fun getPercentOfRightAnswers(gameResult: GameResult) : Int {
-    return if (gameResult.countOfQuestions == 0) {
-        0
+
+@BindingAdapter("enoughPercent")
+fun bindEnoughPercent(progressBar: ProgressBar, enough: Boolean) {
+    val color = getColorByState(progressBar.context, enough)
+    progressBar.progressTintList = ColorStateList.valueOf(color)
+}
+
+private fun getColorByState(context: Context, goodState: Boolean): Int {
+    val colorResId = if (goodState) {
+        android.R.color.holo_green_light
     } else {
-        ((gameResult.countOfRightAnswers/gameResult.countOfQuestions.toDouble())*100).toInt()
+        android.R.color.holo_red_light
+    }
+    return ContextCompat.getColor(context, colorResId)
+}
+
+@BindingAdapter("numberAsText")
+fun bindNumberAsText(textView: TextView, number: Int) {
+    textView.text = number.toString()
+}
+
+@BindingAdapter("onOptionClickListener")
+fun bindOnOptionClickListener(textView: TextView, clickListener: OnOptionClickListener) {
+    textView.setOnClickListener {
+        clickListener.onOptionClick(textView.text.toString().toInt())
     }
 }
